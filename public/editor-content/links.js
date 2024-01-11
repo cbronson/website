7e20d72256cc42c8b46efbf29e52bd76fd714d46
Connor Bronson <connorbronson@gmail.com>
2024-01-11 17:12:12 -0500

    changes

--- public/editor-content/links.js
+++ public/editor-content/links.js
@@ -1,9 +1,8 @@
 // links
 
 export const links = [
-    '["mbp.repair"](https://mbp.repair)',
-    '["LinkedIn"](https://www.linkedin.com/in/connor-bronson)',
     '["GitHub"](https://github.com/cbronson)',
+    '["LinkedIn"](https://www.linkedin.com/in/connor-bronson)',
     '["Twitter"](https://twitter.com/connorbronson)',
     '["StackOverflow"](https://stackoverflow.com/users/1698273/cbronson)',
 ]
